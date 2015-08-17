a2f2844b8b6f98a87f91cc3cd9815eeca7e3a4ac
Haiyan B <Durrah91@gmail.com>
2015-08-17 23:59:07 +0800

    added new pictures

--- src/logic/Controller.java
+++ src/logic/Controller.java
@@ -15,14 +15,14 @@ public class Controller {
 	private Card c3;
 	
 	public void draw(){			
-		turn++;
 		int ti;
-		if (turn%2==1){
+		if (turn%2==0){
 			ti = 0;
 		} else {
 			ti = 1;
 		}
 		addCard(ti, mydeck[turn]);
+		turn++;
 	}
 	
 	private void addCard(int pi, Card pCard){
@@ -102,59 +102,71 @@ public class Controller {
 		c2 = null;
 		c3 = null;
 		
-		ImageIcon tmp = new ImageIcon("image/ace.png");
+		ImageIcon i1 = new ImageIcon("image/1.jpg");
+		ImageIcon i2 = new ImageIcon("image/2.jpg");
+		ImageIcon i3 = new ImageIcon("image/3.jpg");
+		ImageIcon i4 = new ImageIcon("image/4.jpg");
+		ImageIcon i5 = new ImageIcon("image/5.jpg");
+		ImageIcon i6 = new ImageIcon("image/6.jpg");
+		ImageIcon i7 = new ImageIcon("image/7.jpg");
+		ImageIcon i8 = new ImageIcon("image/8.jpg");
+		ImageIcon i9 = new ImageIcon("image/9.jpg");
+		ImageIcon i10 = new ImageIcon("image/10.jpg");
+		ImageIcon ij = new ImageIcon("image/j.jpg");
+		ImageIcon iq = new ImageIcon("image/q.jpg");
+		ImageIcon ik = new ImageIcon("image/k.jpg");
 		
-		mydeck[0] = new Card(7, tmp);
-		mydeck[1] = new Card(1, tmp);
-		mydeck[2] = new Card(9, tmp);  
-		mydeck[3] = new Card(1, tmp);
-		mydeck[4] = new Card(2, tmp);
-		mydeck[5] = new Card(5, tmp);
-		mydeck[6] = new Card(9, tmp);
-		mydeck[7] = new Card(6, tmp);
-		mydeck[8] = new Card(1, tmp);
-		mydeck[9] = new Card(10, tmp);
-		mydeck[10] = new Card(4, tmp);
-		mydeck[11] = new Card(4, tmp);
-		mydeck[12] = new Card(8, tmp);
-		mydeck[13] = new Card(5, tmp);
-		mydeck[14] = new Card(3, tmp);
-		mydeck[15] = new Card(10, tmp);
-		mydeck[16] = new Card(4, tmp);
-		mydeck[17] = new Card(10, tmp);
-		mydeck[18] = new Card(10, tmp);
-		mydeck[19] = new Card(7, tmp);
-		mydeck[20] = new Card(6, tmp);
-		mydeck[21] = new Card(2, tmp);
-		mydeck[22] = new Card(5, tmp);
-		mydeck[23] = new Card(8, tmp);
-		mydeck[24] = new Card(10, tmp);
-		mydeck[25] = new Card(10, tmp);
-		mydeck[26] = new Card(3, tmp);
-		mydeck[27] = new Card(10, tmp);
-		mydeck[28] = new Card(10, tmp);
-		mydeck[29] = new Card(6, tmp);
-		mydeck[30] = new Card(5, tmp);
-		mydeck[31] = new Card(7, tmp);
-		mydeck[32] = new Card(2, tmp);
-		mydeck[33] = new Card(8, tmp);
-		mydeck[34] = new Card(6, tmp);
-		mydeck[35] = new Card(3, tmp);
-		mydeck[36] = new Card(1, tmp);
-		mydeck[37] = new Card(3, tmp);
-		mydeck[38] = new Card(10, tmp);
-		mydeck[39] = new Card(2, tmp);
-		mydeck[40] = new Card(10, tmp);
-		mydeck[41] = new Card(4, tmp);
-		mydeck[42] = new Card(7, tmp);
-		mydeck[43] = new Card(9, tmp);
-		mydeck[44] = new Card(10, tmp);
-		mydeck[45] = new Card(10, tmp);
-		mydeck[46] = new Card(10, tmp);
-		mydeck[47] = new Card(10, tmp);
-		mydeck[48] = new Card(10, tmp);
-		mydeck[49] = new Card(10, tmp);
-		mydeck[50] = new Card(8, tmp);
-		mydeck[51] = new Card(9, tmp);
+		mydeck[0] = new Card(7, i7);
+		mydeck[1] = new Card(1, i1);
+		mydeck[2] = new Card(9, i9);  
+		mydeck[3] = new Card(1, i1);
+		mydeck[4] = new Card(2, i2);
+		mydeck[5] = new Card(5, i5);
+		mydeck[6] = new Card(9, i9);
+		mydeck[7] = new Card(6, i6);
+		mydeck[8] = new Card(1, i1);
+		mydeck[9] = new Card(10, i10);
+		mydeck[10] = new Card(4, i4);
+		mydeck[11] = new Card(4, i4);
+		mydeck[12] = new Card(8, i8);
+		mydeck[13] = new Card(5, i5);
+		mydeck[14] = new Card(3, i3);
+		mydeck[15] = new Card(10, i10);
+		mydeck[16] = new Card(4, i4);
+		mydeck[17] = new Card(10, i10);
+		mydeck[18] = new Card(10, i10);
+		mydeck[19] = new Card(7, i7);
+		mydeck[20] = new Card(6, i6);
+		mydeck[21] = new Card(2, i2);
+		mydeck[22] = new Card(5, i5);
+		mydeck[23] = new Card(8, i8);
+		mydeck[24] = new Card(10, ik);
+		mydeck[25] = new Card(10, iq);
+		mydeck[26] = new Card(3, i3);
+		mydeck[27] = new Card(10, ij);
+		mydeck[28] = new Card(10, ik);
+		mydeck[29] = new Card(6, i6);
+		mydeck[30] = new Card(5, i5);
+		mydeck[31] = new Card(7, i7);
+		mydeck[32] = new Card(2, i2);
+		mydeck[33] = new Card(8, i8);
+		mydeck[34] = new Card(6, i6);
+		mydeck[35] = new Card(3, i3);
+		mydeck[36] = new Card(1, i1);
+		mydeck[37] = new Card(3, i3);
+		mydeck[38] = new Card(10, i10);
+		mydeck[39] = new Card(2, i2);
+		mydeck[40] = new Card(10, i10);
+		mydeck[41] = new Card(4, i4);
+		mydeck[42] = new Card(7, i7);
+		mydeck[43] = new Card(9, i9);
+		mydeck[44] = new Card(10, i10);
+		mydeck[45] = new Card(10, ik);
+		mydeck[46] = new Card(10, ik);
+		mydeck[47] = new Card(10, iq);
+		mydeck[48] = new Card(10, ij);
+		mydeck[49] = new Card(10, iq);
+		mydeck[50] = new Card(8, i8);
+		mydeck[51] = new Card(9, i9);
 	}
 }
